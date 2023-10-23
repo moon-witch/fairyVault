3d3273392ac5acbba29852233d98e3c01ce696c2
langenbach <langenbach@vilisto.de>
2023-10-23 13:21:01 +0200

    added manifest, robots.txt, added icons and edited index.html

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,40 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['fairy-icon.png', 'fairy-icon192x192.png', 'fairy-icon512x512.png', 'fairy-icon-180x180.png'],
+      manifest: {
+        name: 'fairyVault',
+        short_name: 'fairyVault',
+        description: "fairy's vault",
+        theme_color: '#7F6D88',
+        icons: [
+          {
+            src: 'fairy-icon192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'fairy-icon512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'fairy-icon512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'fairy-icon512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
